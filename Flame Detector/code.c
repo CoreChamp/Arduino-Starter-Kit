059db7da23c84f6b47e5bf2c45eefc4a84251b19
CoreChamp <121996992+CoreChamp@users.noreply.github.com>
2023-09-20 11:59:10 +0530

    Update code.c

--- Flame Detector/code.c
+++ Flame Detector/code.c
@@ -1,1 +1,28 @@
+// innova8 || Arduino Starter kit || Flame detector 
+int sensorvalue = 0; 
+int buzzer = 6; Attach buzzer at pin number 6
 
+void setup() {
+  pinMode(8,INPUT); 
+  Serial.begin(9600);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  sensorvalue = digitalRead(8);
+  Serial.print("Sensor value =");
+  Serial.print(sensorvalue);
+  Serial.print('\n');
+  delay(1000);
+  
+  if(sensorvalue == 1){
+  Serial.println("Warning FIRE");
+  digitalWrite(buzzer,HIGH);
+  delay(100);
+  }
+  else{
+    Serial.println("No Flames");
+    digitalWrite(buzzer,LOW);
+      delay(100);
+  }
+}
